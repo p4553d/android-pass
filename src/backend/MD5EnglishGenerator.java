59b666088b31cb501b00c23493e84dd12f40f5d4
p4553d <p4553d@googlemail.com>
2014-06-12 01:23:52 +0200

    Bugfix: original application calculates md5 on hex representation of strings. Plus additional raw loop in humanize.

--- src/backend/MD5EnglishGenerator.java
+++ src/backend/MD5EnglishGenerator.java
@@ -15,29 +15,44 @@ public class MD5EnglishGenerator extends AbstractEnglishGenerator implements
 
     private static int rounds = 10;
 
-    private byte[] concatenate(byte[] a, byte[] b) {
+    private static byte[] concatenate(byte[] a, byte[] b) {
 	int aLen = a.length;
 	int bLen = b.length;
 
 	byte[] res = new byte[aLen + bLen];
 
 	System.arraycopy(a, 0, res, 0, aLen);
-	System.arraycopy(a, 0, res, aLen, bLen);
+	System.arraycopy(b, 0, res, aLen, bLen);
 
 	return res;
     }
 
-    private byte[] extract(byte[] a, int start, int stop) {
+    private static byte[] extract(byte[] a, int start, int stop) {
 	// TODO: some checks was useful
 
 	byte[] res = new byte[stop - start + 2];
-	for (int i = start; i < stop; i++) {
-	    res[i + 1] = a[start + i];
-	}
+
 	res[0] = 0; // suppress negative numbers
 
-	return res;
+	for (int i = 0; i < stop - start + 1; i++) {
+	    res[i + 1] = a[start + i];
+	}
 
+	return res;
+    }
+
+    private static byte[] hexify(byte[] bytes) {
+	char[] hexArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F' };
+	byte[] hexChars = new byte[bytes.length * 2];
+	int v;
+
+	for (int j = 0; j < bytes.length; j++) {
+	    v = bytes[j] & 0xFF;
+	    hexChars[j * 2] = (byte) hexArray[v >>> 4];
+	    hexChars[j * 2 + 1] = (byte) hexArray[v & 0x0F];
+	}
+	return hexChars;
     }
 
     @Override
@@ -57,22 +72,23 @@ public class MD5EnglishGenerator extends AbstractEnglishGenerator implements
 	} catch (NoSuchAlgorithmException e) {
 	    e.printStackTrace();
 	}
+
 	// hash master password and site in multiple rounds
 	for (int i = 0; i < rounds; i++) {
-	    current = md.digest(current);
+	    current = hexify(md.digest(current));
 	}
+
 	current = concatenate(current, siteBytes);
+
 	for (int i = 0; i < rounds; i++) {
-	    current = md.digest(current);
+	    current = hexify(md.digest(current));
 	}
+	current = md.digest(current);
 
 	// produce seed (long) out of hash
-	byte[] ext = extract(current, 0, 7);
-	res = new BigInteger(ext);
+	byte[] ext = extract(current, 11, 15); // TODO: get rid of magic numbers
 
-	// mask some of them
-	// long mask = (0xffffffffffffffffL) >> 24;
-	// res &= mask;
+	res = new BigInteger(ext);
 
 	return res;
     }
--- src/frontend/terminalFrontend.java
+++ src/frontend/terminalFrontend.java
@@ -15,7 +15,9 @@ public class terminalFrontend {
      */
     public static void main(String[] args) {
 	MD5EnglishGenerator gen = new MD5EnglishGenerator();
-	System.out.print(gen.generatePassword("12345678901", "google.de"));
+	System.out.print(gen.generatePassword("12345678901", "google.de")); // theitheuseeingerms-463
+	System.out.println();
+	System.out.print(gen.generatePassword("abcdefghijk", "google.de")); // spalay-nys-191
     }
 
 }
